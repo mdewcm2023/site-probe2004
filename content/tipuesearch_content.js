2fbeba2068597857aff2fef98247a8d45154922b
unknown <joe012299@gmail.com>
2023-04-17 20:55:02 +0800

    add cmsimde

---
+++ content/tipuesearch_content.js
@@ -0,0 +1,1 @@
+var tipuesearch = {"pages": [{'title': 'About', 'text': '網站: https://mdewcm2023.github.io/site-probe2004/ \xa0 \n 倉儲: https://github.com/mdewcm2023/site-probe2004 \xa0 \n', 'tags': '', 'url': 'About.html'}, {'title': 'w9', 'text': '', 'tags': '', 'url': 'w9.html'}]};
